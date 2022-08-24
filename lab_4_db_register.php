fcd3bb4e088ef2e231c3eeda71a295d23e5b4a8a
TroJ4nZ <trojanz1258@gmail.com>
2022-08-24 17:56:17 +0200

    Add database register code

--- lab_4_db_register.php
+++ lab_4_db_register.php
@@ -1,9 +1,3 @@
-<?php
-
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -26,25 +20,31 @@
                         <label for=name>Name</label>
                     </li>
                     <li>
-                        <input type=text maxlength="50" required>
+                        <input type=text maxlength="50" id=name name=name required>
                     </li>
                     <li>
                         <label for=name>Email</label>
                     </li>
                     <li>
-                        <input type=email maxlength="50" required>
+                        <input type=email maxlength="50" id=email name=email required>
                     </li>
                     <li>
                         <label for=gender>Gender</label>
                     </li>
                     <li>
-                        <input type=radio id=gender name=gender value=1 required>Male
+                        <label>
+                            <input type=radio id=gender name=gender value=1 required>Male
+                        </label>
                     </li>
                     <li>
-                        <input type=radio id=gender name=gender value=0 required>Female
+                        <label>
+                            <input type=radio id=gender name=gender value=0 required>Female
+                        </label>
                     </li>
                     <li>
-                        <input type=checkbox id=mails name=mails required>Receieve E-mails from us
+                        <label>
+                            <input type=checkbox id=mails name=mails required>Receieve E-mails from us
+                        </label>
                     </li>
                     <li>
                         <input type=submit class=btn id=submit_btn name=submit_btn>
@@ -57,3 +57,42 @@
 </body>
 
 </html>
+
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "iti_lab";
+$dbport = "3306";
+$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname, $dbport);
+if (!$con) {
+    echo "Error: Unable to conect to MySQL." . PHP_EOL;
+    echo "Debugging error #: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    die;
+} else {
+    if (isset($_POST["submit_btn"])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $mails = (isset($_POST['mails'])) ? "1" : "0";
+
+        $dbgender = ($_POST['gender'] == "1") ? "Male" : "Female";
+
+        $stmt = "INSERT INTO users(name, email, gender, mails) VALUES(?,?,?,?)";
+        $ps = mysqli_prepare($con, $stmt);
+
+        mysqli_stmt_bind_param($ps, "sssi", $name, $email, $dbgender, $mails);
+        if (!mysqli_stmt_execute($ps)) {
+            echo "Debugging error #: " . mysqli_errno($con) . PHP_EOL;
+            echo "Debugging error: " . mysqli_error($con) . PHP_EOL;
+            die;
+        } else {
+            echo "Success! Redirecting you...";
+            header("refresh: 3; url=lab_4_db_register.php");
+        }
+    }
+}
+mysqli_close($con);
+
+?>
