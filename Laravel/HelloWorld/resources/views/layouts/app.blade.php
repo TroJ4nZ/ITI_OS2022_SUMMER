047efa32465080133934aa302f564c48bfd1659e
TroJ4nZ <trojanz1258@gmail.com>
2022-08-27 13:16:27 +0200

    Include navbar layout and add bootstrap

---
+++ Laravel/HelloWorld/resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+{{-- resources/views/layouts/app.blade.php --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ITI Laravel Day 2 - @yield('title')</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body>
+   @section('navbar')
+        @include('layouts.navbar')
+   @show 
+
+   @section('content')
+
+   @show
+</body>
+</html>
