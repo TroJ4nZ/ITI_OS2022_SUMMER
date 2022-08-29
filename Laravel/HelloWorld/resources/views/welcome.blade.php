3619b616b34b575d81022671ee342d4f4ff760f0
TroJ4nZ <trojanz1258@gmail.com>
2022-08-29 20:37:10 +0200

    Add @extend and @section tags for welcome view from main app.blade.php layout

--- Laravel/HelloWorld/resources/views/welcome.blade.php
+++ Laravel/HelloWorld/resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
+@extends('layouts.app')
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -21,6 +22,7 @@
         </style>
     </head>
     <body class="antialiased">
+        @section('content')
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
@@ -130,3 +132,4 @@
         </div>
     </body>
 </html>
+@endsection
