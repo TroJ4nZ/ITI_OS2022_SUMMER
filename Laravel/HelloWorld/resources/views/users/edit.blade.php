12231d482102197dde266c6c79cd60527de476ed
TroJ4nZ <trojanz1258@gmail.com>
2022-08-31 20:19:42 +0200

    Add name attributes for Email & Name inputs in users.edit form

--- Laravel/HelloWorld/resources/views/users/edit.blade.php
+++ Laravel/HelloWorld/resources/views/users/edit.blade.php
@@ -3,18 +3,18 @@
 
 @section('content')
     @parent
-    <form action="{{ route('users.update', ['id' => $user['id']]) }}">
+    <form method=POST action="{{ route('users.update', ['id' => $user['id']]) }}">
         @method('PUT')
         @csrf
         <label>Editing user with id {{$user["id"]}}</label>
 
         <div class="form-group">
           <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{$user['email']}}">
+          <input type="email" class="form-control" id="exampleInputEmail1" name=email aria-describedby="emailHelp" value="{{$user['email']}}">
         </div>
         <div class="form-group">
           <label for="exampleInputPassword1">Name</label>
-          <input type="text" class="form-control" id="exampleInputPassword1" value="{{$user['name']}}">
+          <input type="text" class="form-control" id="exampleInputPassword1" name=name value="{{$user['name']}}">
         </div>
         <div class="form-group form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1">
