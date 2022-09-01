7c4274312563920aaabd08d5170cba2f967ac396
TroJ4nZ <trojanz1258@gmail.com>
2022-09-01 11:32:51 +0200

    Create seeder & factory for Post model

--- Laravel/HelloWorld/database/seeders/DatabaseSeeder.php
+++ Laravel/HelloWorld/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(PostSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
