40d7d1a67fa549b90029cee40cffd7bafcc597cb
TroJ4nZ <trojanz1258@gmail.com>
2022-09-01 15:42:33 +0200

    Add routing for store, update, softs and restore

--- Laravel/HelloWorld/routes/web.php
+++ Laravel/HelloWorld/routes/web.php
@@ -52,11 +52,14 @@ Posts Routes
 
 Route::get('posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('posts/{id}', [PostController::class, 'show'])->where('id', "[0-9]+")->name('posts.show');
 Route::get('posts/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::post('posts/{id}', [PostController::class, 'update'])->name('posts.update');
+Route::put('posts/{id}', [PostController::class, 'update'])->name('posts.update');
 Route::delete('posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::get('posts/deleted', [PostController::class, 'destroyed'])->name('posts.destroyed');
+Route::get('posts/softs', [PostController::class, 'softs'])->name('posts.softs');
+Route::patch('posts/{id}', [PostController::class, 'restore'])->name('posts.restore');
+
 
 
 // Route --> A class
