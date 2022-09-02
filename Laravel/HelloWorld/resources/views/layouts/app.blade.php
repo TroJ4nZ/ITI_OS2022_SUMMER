47cbd2e56698a118db9d8df4aa18ee62f1095a22
TroJ4nZ <trojanz1258@gmail.com>
2022-09-02 13:00:41 +0200

    Add container div

--- Laravel/HelloWorld/resources/views/layouts/app.blade.php
+++ Laravel/HelloWorld/resources/views/layouts/app.blade.php
@@ -9,18 +9,20 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 <body>
-    {{-- @php 
+    {{-- @php
         use Illuminate\Http\Request;
     @endphp --}}
 
    @section('navbar')
         @include('layouts.navbar')
-   @show 
+   @show
 
+   <div class="container">
    @section('content')
 
    @show
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
+   </div>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>
