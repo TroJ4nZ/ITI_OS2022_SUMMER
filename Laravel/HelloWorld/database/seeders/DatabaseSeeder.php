c8a9bdeb836a34e2e6c3f14b548fdecd100f86e4
TroJ4nZ <trojanz1258@gmail.com>
2022-09-02 16:52:43 +0200

    Create & run seeder for users table

--- Laravel/HelloWorld/database/seeders/DatabaseSeeder.php
+++ Laravel/HelloWorld/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PostSeeder::class);
+        // $this->call(PostSeeder::class);
+        $this->call(UserSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
