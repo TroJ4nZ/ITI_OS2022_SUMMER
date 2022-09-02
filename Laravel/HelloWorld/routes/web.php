6d4d1d79b2aeb9fb6fc2cbe9453d123fed9b1569
TroJ4nZ <trojanz1258@gmail.com>
2022-09-02 18:05:11 +0200

    Move dashboard view into resources/users and adjust routing

--- Laravel/HelloWorld/routes/web.php
+++ Laravel/HelloWorld/routes/web.php
@@ -23,8 +23,8 @@ Route::get('/', function () {
 //     return view('welcome');
 // })->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('users/dashboard', function () {
+    return view('users/dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
