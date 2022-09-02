606037c7d0d649a118d8772844c8d4592f92561d
TroJ4nZ <trojanz1258@gmail.com>
2022-09-02 18:17:14 +0200

    Add list of posts column for each user

--- Laravel/HelloWorld/resources/views/users/show.blade.php
+++ Laravel/HelloWorld/resources/views/users/show.blade.php
@@ -2,32 +2,46 @@
 @section('title', 'User Page')
 
 @section('content')
+    <div class="container">
 
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Handle</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>
-                    {{ $user['id'] }}
-                </td>
-                <td>
-                    {{ $user['name'] }}
-                </td>
-                <td>
-                    {{ $user['email'] }}
-                </td>
-                <td>
-                    {{ $user['email_verified_at'] }}
-                </td>
-            </tr>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Email Verified At</th>
+                    <th scope="col">List of Post Titles</th>
 
-        </tbody>
-    </table>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        {{ $user['id'] }}
+                    </td>
+                    <td>
+                        {{ $user['name'] }}
+                    </td>
+                    <td>
+                        {{ $user['email'] }}
+                    </td>
+                    <td>
+                        {{ $user['email_verified_at'] }}
+                    </td>
+                    <td>
+                        <table class="table table-borderless">
+                            @foreach ($user->posts()->get() as $post)
+                                <tr>
+                                    <td>
+                                        {{ $post['title'] }}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 @endsection
