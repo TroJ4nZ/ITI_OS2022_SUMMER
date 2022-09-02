cf9c55bbe939ffc535a1672edf5d8067d389512d
TroJ4nZ <trojanz1258@gmail.com>
2022-09-02 16:49:32 +0200

    Install Breeze for authentication using composer

---
+++ Laravel/HelloWorld/routes/web copy.php
@@ -0,0 +1,77 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*
+    ----------------------------------------------------------------
+  HOME ROUTES
+  ----------------------------------------------------------------
+   */
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+/*
+|
+|--------------------------------------------------------------------------
+User Routes
+|-------------------------------------------------------------
+*/
+
+Route::get('users', [UserController::class, 'index'])->name('users.index');
+Route::get('users/create', [UserController::class, 'create'])->name('users.create');;
+Route::post('users', [UserController::class, 'store'])->name('users.store');
+Route::get('users/{id}', [UserController::class, 'show'])->where('id', "[0-9]+")->name('users.show'); // regex to prevent strings
+Route::get('users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::put('users/{id}', [UserController::class, 'update'])->name('users.update');
+Route::delete('users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+
+
+/*
+|
+|--------------------------------------------------------------------------
+Posts Routes
+|-------------------------------------------------------------
+*/
+
+
+Route::get('posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('posts/{id}', [PostController::class, 'show'])->where('id', "[0-9]+")->name('posts.show');
+Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::put('posts/{id}', [PostController::class, 'update'])->name('posts.update');
+Route::delete('posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+Route::get('posts/softs', [PostController::class, 'softs'])->name('posts.softs');
+Route::patch('posts/{id}', [PostController::class, 'restore'])->name('posts.restore');
+
+
+
+// Route --> A class
+// :: --> scope resolution operator (for static methods and properties of a class)
+// get --> static method
+// => --> for array
+// -> --> dot operator for both methods and properties
+// Route::get returns route, we then call the method of Route that is name() using the
+// access dot operator -> (method chaining)
+
+
+
+Route::fallback(function () {
+    return "This is a fallback route";
+});
