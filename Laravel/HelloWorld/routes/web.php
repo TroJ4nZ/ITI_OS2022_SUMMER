ceddb92c01987559396cff1030e8e95e9b6ba48d
TroJ4nZ <trojanz1258@gmail.com>
2022-09-02 13:00:17 +0200

    Change page edit route to accept {post} instead of {id}

--- Laravel/HelloWorld/routes/web.php
+++ Laravel/HelloWorld/routes/web.php
@@ -54,7 +54,7 @@ Route::get('posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('posts/{id}', [PostController::class, 'show'])->where('id', "[0-9]+")->name('posts.show');
-Route::get('posts/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
 Route::put('posts/{id}', [PostController::class, 'update'])->name('posts.update');
 Route::delete('posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
 Route::get('posts/softs', [PostController::class, 'softs'])->name('posts.softs');
