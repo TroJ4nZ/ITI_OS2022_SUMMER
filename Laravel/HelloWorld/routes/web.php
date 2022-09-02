a407cda05ae0a938c2771fc46a3a1e2dbcc7d9dd
TroJ4nZ <trojanz1258@gmail.com>
2022-09-02 18:16:51 +0200

    Move dashboard back to views and adjust routes

--- Laravel/HelloWorld/routes/web.php
+++ Laravel/HelloWorld/routes/web.php
@@ -23,8 +23,8 @@ Route::get('/', function () {
 //     return view('welcome');
 // })->name('home');
 
-Route::get('users/dashboard', function () {
-    return view('users/dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
