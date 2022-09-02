3928840b049f1687014439ecffad688880883ddc
TroJ4nZ <trojanz1258@gmail.com>
2022-09-02 17:12:00 +0200

    Add container div and style button text-dark

--- Laravel/HelloWorld/resources/views/posts/edit.blade.php
+++ Laravel/HelloWorld/resources/views/posts/edit.blade.php
@@ -2,19 +2,21 @@
 @section('title', 'Edit Post')
 
 @section('content')
-    @parent
-    <form method="POST" action="{{route('posts.update', ['id' => $post["id"]])}}">
-        @method("PUT")
-        @csrf
-        <label>Editing post with id {{$post["id"]}}</label>
-        <div class="form-group">
-          <label for="title">Title</label>
-          <input type="text" class="form-control" id="title" name="title" value={{$post["title"]}}>
-        </div>
-        <div class="form-group">
-          <label for="body">Body</label>
-          <textarea type="textarea" class="form-control" id="body" name="body" cols=25 rows=5>{{$post["body"]}}</textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
+    <div class="container">
+
+        <form method="POST" action="{{ route('posts.update', ['id' => $post['id']]) }}">
+            @method('PUT')
+            @csrf
+            <label>Editing post with id {{ $post['id'] }}</label>
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" class="form-control" id="title" name="title" value={{ $post['title'] }}>
+            </div>
+            <div class="form-group">
+                <label for="body">Body</label>
+                <textarea type="textarea" class="form-control" id="body" name="body" cols=25 rows=5>{{ $post['body'] }}</textarea>
+            </div>
+            <button type="submit" class="btn btn-primary text-dark">Submit</button>
+        </form>
+    </div>
 @endsection
