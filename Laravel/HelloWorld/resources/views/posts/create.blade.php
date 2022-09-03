de5b0791b7b750e6fed7957de3869e265f1be997
TroJ4nZ <trojanz1258@gmail.com>
2022-09-03 23:02:42 +0200

    Add input for post image upload

--- Laravel/HelloWorld/resources/views/posts/create.blade.php
+++ Laravel/HelloWorld/resources/views/posts/create.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     @parent
     <div class="container">
-    <form method=POST action={{ route('posts.store') }}>
+    <form method=POST action={{ route('posts.store') }} enctype="multipart/form-data">
         @method('POST')
         @csrf
         <div class="form-group">
@@ -15,6 +15,11 @@
             <label for="body">Body</label>
             <textarea type="textarea" class="form-control" id="body" name="body" cols=25 rows=5
                 placeholder="Enter the post body"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="image">Upload Image</label>
+            <input type="file" class="form-control" id="image" name="image"></input>
+
         </div>
         <button type="submit" class="btn btn-primary text-dark">Submit</button>
     </form>
